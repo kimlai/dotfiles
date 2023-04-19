c51bd42b9d6ed6dd12c711ef4c2ee02a4497458d
Kim Laï Trinh <kimlai.trinh.external@zf.com>
2023-04-19 14:03:17 +0200

    [kitty] add default value for new session name

--- kitty/session_renamer.py
+++ kitty/session_renamer.py
@@ -1,6 +1,7 @@
 import json
+import os
 
-from os.path import expanduser
+from os.path import expanduser, basename
 from typing import List
 from kitty.boss import Boss
 from kittens.tui.operations import styled
@@ -9,11 +10,12 @@ from kitty.fast_data_types import current_focused_os_window_id
 def main(args: List[str]) -> str:
     # this is the main entry point of the kitten, it will be executed in
     # the overlay window when the kitten is launched
+    default = basename(os.environ['PWD'])
     print(styled('Enter the new title for this session below.', bold=True))
-    answer = input('> ')
+    answer = input(f'({default}) > ')
     # whatever this function returns will be available in the
     # handle_result() function
-    return answer
+    return answer or default
 
 def handle_result(args: List[str], answer: str, target_window_id: int, boss: Boss) -> None:
     with open(f"{expanduser('~')}/.kitty-sessions.json") as f:
