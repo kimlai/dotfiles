3c7f4445bde89e0765578bc6ea5d9442c4825249
Kim Laï Trinh <kimlai.trinh.external@zf.com>
2023-04-25 11:07:10 +0200

    [nvim] show scala+dap test results in diagnostics

--- init.lua
+++ init.lua
@@ -211,7 +211,7 @@ vim.keymap.set("n", "<leader>dl", function() require("dap").run_last() end)
 ------------------------------
 metals_config = require("metals").bare_config()
 metals_config.init_options.statusBarProvider = "on"
-metals_config.enableSemanticHighlighting = true
+metals_config.settings = { testUserInterface = "Test Explorer" }
 local dap = require("dap")
 dap.configurations.scala = {
   {
@@ -232,8 +232,71 @@ dap.configurations.scala = {
     },
   },
 }
+
+local handle_test_start = function(session, body)
+  -- Clear the test result extmarks.
+  -- this should be done on the actual buffer containing the test defintion, and only when starting tests, not
+  -- when launching debugger, but I can't figure out how.
+  local bufnr = vim.api.nvim_get_current_buf()
+  local namespace = vim.api.nvim_create_namespace("dap-test-results")
+  vim.diagnostic.reset(namespace, bufnr)
+  vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
+end
+
+local handle_test_result = function(session, body)
+  local namespace = vim.api.nvim_create_namespace("dap-test-results")
+  local suite = nil
+  local suite_bufnr = nil
+  for _, project in pairs(metals_get_tests()) do
+    for _, registered_suite in pairs(project.suites) do
+      if registered_suite.fullyQualifiedClassName == body.data.suiteName then
+        suite = registered_suite
+        for _, buf in ipairs(vim.fn.getbufinfo()) do
+          if suite.location.uri == "file://" .. buf.name then
+            suite_bufnr = buf.bufnr
+          end
+        end
+      end
+    end
+  end
+  for _, test_result in ipairs(body.data.tests) do
+    local test_linenr = nil
+    for _, registered_test in pairs(suite.testCases) do
+      if test_result.testName == registered_test.name then
+        test_linenr = registered_test.location.range['end'].line
+      end
+    end
+    if test_result.kind == "passed" then
+      local text = {"✓", "DiagnosticOk"}
+      vim.api.nvim_buf_set_extmark(suite_bufnr, namespace, test_linenr, 0, { virt_text = { text } })
+    elseif test_result.kind == "skipped" then
+      local text = {"skipped"}
+      vim.api.nvim_buf_set_extmark(suite_bufnr, namespace, test_linenr, 0, { virt_text = { text } })
+    elseif test_result.kind == "failed" then
+      local text = {"⨯", "DiagnosticError"}
+      vim.api.nvim_buf_set_extmark(suite_bufnr, namespace, test_linenr, 0, { virt_text = { text } })
+      vim.diagnostic.set(
+        namespace,
+        suite_bufnr,
+        {{
+          lnum = test_linenr,
+          col = 0,
+          severity = vim.diagnostic.severity.ERROR,
+          source = "dap-run-test",
+          message = test_result.error,
+          user_data = {},
+        }}
+      )
+    end
+  end
+end
+
+dap.listeners.after['event_testResult']['scalaTestResults'] = handle_test_result
+dap.listeners.after['launch']['scalaTestResults'] = handle_test_start
+
 metals_config.on_attach = function(client, bufnr)
   require("metals").setup_dap()
+  dap.listeners.after['event_testResult']['scalaTestResults'] = handle_test_result
 end
 
 metals = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
